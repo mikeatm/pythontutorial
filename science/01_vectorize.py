fd82eaa4f8012ac4261386533f7e8b6ef6be77de
Jeudy Blanco <jeudyx@gmail.com>
2014-11-13 04:34:29 -0600

    Soluytion 1

--- science/01_vectorize.py
+++ science/01_vectorize.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import random 
 import sys
+import time
 
 def fill_list(N):
     """
@@ -14,6 +15,24 @@ def fill_list(N):
         y.append(  math.exp(-x[i])*x[i] ) 
 
 
+def fill_list_np(N):
+    """
+    generate a list of  points obtained from N 
+    consecutive values,  satisfying f(x) :  1/sqrt(2*pi) exp( -(x^2)/2 ) 
+    """
+    x = np.arange(N, dtype='d') / N
+    y = np.exp(x)
+
+
 if __name__ == "__main__":
-    N = int( sys.argv[1]) 
+    N = int(sys.argv[1]) 
+    start = time.time()    
     fill_list(N) 
+    end = time.time()
+    print 'Time taken for pure python: %s' % (end - start)
+
+    start = time.time()    
+    fill_list_np(N) 
+    end = time.time()
+
+    print 'Time taken for numpy: %s' % (end - start)
